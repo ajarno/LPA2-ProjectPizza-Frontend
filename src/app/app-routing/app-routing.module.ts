296d2a002c12ccd3dbfba21c66d33c7494138ae6
Anais Jarno <ajarno@utbm.fr>
2018-10-16 09:40:40 +0200

    clean the code

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PizzaComponent } from '../pizza/pizza.component';
 import { PizzaListComponent }      from '../pizza-list/pizza-list.component';
 import { PizzaDetailComponent } from '../pizza-detail/pizza-detail.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: '/pizza-list', pathMatch: 'full' },
-  { path: 'pizza-list', component: PizzaListComponent },
   { path: '', component: PizzaListComponent },
+  { path: 'pizza-list', component: PizzaListComponent },
   { path: 'pizza-list/:id', component: PizzaDetailComponent },
 ];
+
 @NgModule({
-  imports: [ 
-    // RouterModule,
-    RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
+
 export class AppRoutingModule {}
--- src/app/pizza-detail/pizza-detail.component.ts
+++ src/app/pizza-detail/pizza-detail.component.ts
@@ -2,27 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Pizza } from '../pizza';
 import { ActivatedRoute } from '@angular/router';
 import { PizzaService } from '../pizza.service';
-import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-pizza-detail',
   templateUrl: './pizza-detail.component.html',
   styleUrls: ['./pizza-detail.component.css']
 })
-export class PizzaDetailComponent implements OnInit { 
 
-  public id = + this.activateRoute.snapshot.paramMap.get('id');
+export class PizzaDetailComponent implements OnInit { 
+  
   pizza: Pizza;
 
-  constructor(
-    private activateRoute: ActivatedRoute,
-    // private location: Location,
-    private pizzaService: PizzaService){}
-
+  constructor(private activateRoute: ActivatedRoute, private pizzaService: PizzaService){}
 
   ngOnInit() {
-    this.pizzaService.showPizza(this.id)
+    const id = + this.activateRoute.snapshot.paramMap.get('id');
+
+    this.pizzaService.showPizza(id)
     .subscribe(pizza => this.pizza = pizza);
   }
-
 }
+
+// future optimization: handling error (eg: https://angular.io/tutorial/toh-pt6#error-handling)
